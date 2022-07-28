4fd14463a4e3a38c4980092028a6081f3d50c379
pravda23 <pravda23@gmail.com>
2022-07-28 16:15:21 +0200

    final before react branch

---
+++ to-rename/rename-html.js
@@ -0,0 +1,33 @@
+// renames all .html files in folder to blank file extensions
+
+// run cmd
+// navigate to /jbcom
+
+// gives you capabilities to interact with file system in node
+const fs = require("fs");
+
+function toHTML() {
+  return (
+    fs.promises
+      .readdir(".")
+      .then((stuff) => {
+        // return files ending in .html
+        const result = stuff
+          .filter((e) => {
+            return e.toLowerCase().endsWith(".html");
+          })
+          // truncate .html
+          .map((e) => {
+            return fs.promises.rename(e, e.substring(0, e.length - 5));
+          });
+        console.log(result);
+        return result;
+      })
+      // returns all files
+      .then((e) => {
+        return Promise.all(e);
+      })
+  );
+}
+
+toHTML();
