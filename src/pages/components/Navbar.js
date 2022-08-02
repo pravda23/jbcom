f14ebee8183b2aac69bb45facf179b2d629e9e5e
pravda23 <pravda23@gmail.com>
2022-08-02 11:23:38 +0200

    TS mobile menu state

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,12 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 import "../styles/global.css"
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState([false])
+  let menu
+  if (showMenu) {
+    menu = (
+      <div className="navLinksSide">
+        <li>
+          <a href="/">HOME</a>
+        </li>
+        <li>
+          <a href="/writing">WRITING</a>
+        </li>
+        <li>
+          <a href="/podcasting">PODCASTING</a>
+        </li>
+        <li>
+          <a href="/music">MUSIC</a>
+        </li>
+        <li>
+          <a href="/coding">CODING</a>
+        </li>
+        <li>
+          <a href="/contact">CONTACT</a>
+        </li>
+      </div>
+    )
+  }
   return (
-    <div>
-      <FontAwesomeIcon icon={faBars} />
-    </div>
+    <>
+      <div>
+        <FontAwesomeIcon
+          icon={faBars}
+          className="burger"
+          onClick={() => setShowMenu(!showMenu)}
+        />
+      </div>
+      <div>{menu}</div>
+    </>
   )
 }
